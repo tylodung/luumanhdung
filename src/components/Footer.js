5d96ba5ae10056c55b76dddfa49ee1ef08e14a60
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2020-05-13 19:04:58 +0700

    up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () =>
     <footer className="site-footer outer">
         <div className="site-footer-content inner">
             <section className="copyright">
-                <Link to="/">Chuyện Đời Thật</Link> Copyright © 2020
+                <Link to="/">Anh & Em</Link> Copyright © 2020
             </section>
             <nav>
                 <a className="social-logo" href="https://youtu.be/N4WTi9EHLqA" >
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -31,7 +31,7 @@ const Template = ({ data }) => {
 
       <Navbar />
 
-      <Helmet title={`Chuyện Đời Thật | ${post.frontmatter.title}`} />
+      <Helmet title={`Anh & Em | ${post.frontmatter.title}`} />
 
       <div className="blog-post-header" style={{ backgroundImage: `url(${ post.frontmatter.image })` }}>
 
